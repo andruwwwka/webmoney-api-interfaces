57f3fd8793f65c8e6abd3bb3b6bf54795fb3f312
Andrey Leontyev <andruwwwka@gmail.com>
2020-03-11 01:02:03 +0300

    Added simple tests for the presence of method implementations in the API class

---
+++ tests/test_interfaces_are_exists.py
@@ -0,0 +1,33 @@
+
+def test_x1_is_exists(api):
+    assert hasattr(api, 'x1')
+
+def test_x2_is_exists(api):
+    assert hasattr(api, 'x2')
+
+def test_x3_is_exists(api):
+    assert hasattr(api, 'x3')
+
+def test_x4_is_exists(api):
+    assert hasattr(api, 'x4')
+
+def test_x5_is_exists(api):
+    assert hasattr(api, 'x5')
+
+def test_x6_is_exists(api):
+    assert hasattr(api, 'x6')
+
+def test_x7_is_exists(api):
+    assert hasattr(api, 'x7')
+
+def test_x8_is_exists(api):
+    assert hasattr(api, 'x8')
+
+def test_x9_is_exists(api):
+    assert hasattr(api, 'x9')
+
+def test_x10_is_exists(api):
+    assert hasattr(api, 'x10')
+
+def test_x11_is_not_exists(api):
+    assert not hasattr(api, 'x11')
